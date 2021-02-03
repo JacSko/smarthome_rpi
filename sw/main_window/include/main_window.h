997de2766c9658599ab1c14ec816d22fa3ec7a7b
Jacek Skowronek <jacek.skowronek@aptiv.com>
2021-02-03 16:08:56 +0100

    Repo init

---
+++ sw/main_window/include/main_window.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_WINDOW_H_
+#define _MAIN_WINDOW_H_
+
+#include <QtWidgets/QMainWindow>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+};
+#endif
---
+++ sw/main_window/source/main_window.cpp
@@ -0,0 +1,15 @@
+#include "main_window.h"
+#include "../../gui/ui_main_window.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
