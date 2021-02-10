0d7d8e9f62753e5726447f1aa5758f9a47e897a3
Jacek Skowronek <jacek.skowronek@aptiv.com>
2021-02-10 18:48:09 +0100

    cleanup in CMakes, main function corrected

--- sw/main.cpp
+++ sw/main.cpp
@@ -1,13 +1,21 @@
 #include "main_window.h"
 #include "QtWidgets/QApplication"
 
+#include "Logger.h"
+#include "DataProvider.h"
+#include "SocketDriver.h"
+
 int main(int argc, char *argv[])
 {
+   logger_initialize();
+
    QApplication a(argc, argv);
    MainWindow w;
+   std::unique_ptr<ISocketDriver> sock_driver(new SocketDriver());
+   std::unique_ptr<IDataProvider> data_provider(new DataProvider(w, *sock_driver));
+   data_provider->run("127.0.0.1", 2222, '\n');
+   w.setWindowState(Qt::WindowFullScreen);
    w.show();
 
-   a.exec();
-
-   return 1;
+   return a.exec();
 }
